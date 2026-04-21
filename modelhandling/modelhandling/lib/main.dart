a4469ec26435bba1d0ab926d6ac9d4e0c7595c6f
jeii1 <jayrobang@gmail.com>
2026-04-21 10:06:07 +0800

    robang Commit

--- modelhandling/modelhandling/lib/main.dart
+++ modelhandling/modelhandling/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:modelhandling/model/student_model.dart';
+import 'package:modelhandling/screen/chat_screen.dart';
 import 'package:modelhandling/screen/homepage.dart';
 import 'package:modelhandling/screen/login_screen.dart';
 import 'package:modelhandling/screen/product_screeen.dart';
@@ -9,8 +10,8 @@ import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(url: "https://uffodiqzcxdjwgylbmpp.supabase.co", 
-  anonKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVmZm9kaXF6Y3hkandneWxibXBwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA4ODY5MzUsImV4cCI6MjA4NjQ2MjkzNX0.DtPj9W7eFE14aouZUCRiCd8ZDcaSkVXTFthdZ8rgNNQ",
+  await Supabase.initialize(url: "https://ftajoihtluqxavlpbvsz.supabase.co", 
+  anonKey:"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ0YWpvaWh0bHVxeGF2bHBidnN6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzE4Nzc1NTYsImV4cCI6MjA4NzQ1MzU1Nn0.JBRZse7TURt2CC0gZo4Zs5-ZFlVPzvfmaFWveyslX4g",
   );
 
   runApp(const MyApp());
@@ -27,7 +28,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
-      home: LoginPage(),
+      home: ChatPage(username: 'username',),
       debugShowCheckedModeBanner: false,
     );
   }
